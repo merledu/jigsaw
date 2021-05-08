55439bdaaedf9128862babf2f0a28978342a1def
Hadir Khan <hadirkhan10@gmail.com>
2021-05-08 16:28:48 +0500

    modified the testbench to check for new conditions in bram

--- src/test/scala/jigsaw/rams/fpga/BlockRamTester.scala
+++ src/test/scala/jigsaw/rams/fpga/BlockRamTester.scala
@@ -262,4 +262,219 @@ class BlockRamTester extends FreeSpec with ChiselScalatestTester with MemoryDump
     }
   }
 
+  "write full word, read ms half word in maskable bram" in {
+    val programFile = getFile
+    test(BlockRam.createMaskableRAM(programFile, WishboneConfig(32,32), 1024)).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
+      c.clock.step(2)
+      sendRequest(0.U, "habcdef01".U, "b1111".U, true.B)
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      while(c.io.rsp.valid.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      println("Got the response now sending a read request")
+
+      c.clock.step(2)
+      sendRequest(0.U, 0.U, "b1100".U, false.B)
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+
+
+      while(c.io.rsp.valid.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      println("Got the response now reading expected data")
+      c.io.rsp.bits.dataResponse.expect("habcd0000".U)
+
+      println("EXPECTED DATA IS: " + "habcd0000".U.litValue().toInt.toString + " GOT " + c.io.rsp.bits.dataResponse.peek().litValue().toInt.toString)
+      c.clock.step(8)
+
+      def sendRequest(addr: UInt, data: UInt, byteLane: UInt, isWrite: Bool): Unit = {
+        c.clock.step(1)
+        c.io.req.valid.poke(true.B)
+        c.io.req.bits.addrRequest.poke(addr)
+        c.io.req.bits.dataRequest.poke(data)
+        c.io.req.bits.activeByteLane.poke(byteLane)
+        c.io.req.bits.isWrite.poke(isWrite)
+        c.clock.step(1)
+        c.io.req.valid.poke(false.B)
+      }
+    }
+  }
+
+  "write full word, read lsb in maskable bram" in {
+    val programFile = getFile
+    test(BlockRam.createMaskableRAM(programFile, WishboneConfig(32,32), 1024)).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
+      c.clock.step(2)
+      sendRequest(0.U, "habcdef01".U, "b1111".U, true.B)
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      while(c.io.rsp.valid.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      println("Got the response now sending a read request")
+
+      c.clock.step(2)
+      sendRequest(0.U, 0.U, "b0001".U, false.B)
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+
+
+      while(c.io.rsp.valid.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      println("Got the response now reading expected data")
+      c.io.rsp.bits.dataResponse.expect("h00000001".U)
+
+      println("EXPECTED DATA IS: " + "h00000001".U.litValue().toInt.toString + " GOT " + c.io.rsp.bits.dataResponse.peek().litValue().toInt.toString)
+      c.clock.step(8)
+
+      def sendRequest(addr: UInt, data: UInt, byteLane: UInt, isWrite: Bool): Unit = {
+        c.clock.step(1)
+        c.io.req.valid.poke(true.B)
+        c.io.req.bits.addrRequest.poke(addr)
+        c.io.req.bits.dataRequest.poke(data)
+        c.io.req.bits.activeByteLane.poke(byteLane)
+        c.io.req.bits.isWrite.poke(isWrite)
+        c.clock.step(1)
+        c.io.req.valid.poke(false.B)
+      }
+    }
+  }
+
+  "write full word, read msb in maskable bram" in {
+    val programFile = getFile
+    test(BlockRam.createMaskableRAM(programFile, WishboneConfig(32,32), 1024)).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
+      c.clock.step(2)
+      sendRequest(0.U, "habcdef01".U, "b1111".U, true.B)
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      while(c.io.rsp.valid.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      println("Got the response now sending a read request")
+
+      c.clock.step(2)
+      sendRequest(0.U, 0.U, "b1000".U, false.B)
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+
+
+      while(c.io.rsp.valid.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      println("Got the response now reading expected data")
+      c.io.rsp.bits.dataResponse.expect("hab000000".U)
+
+      println("EXPECTED DATA IS: " + "hab000000".U.litValue().toInt.toString + " GOT " + c.io.rsp.bits.dataResponse.peek().litValue().toInt.toString)
+      c.clock.step(8)
+
+      def sendRequest(addr: UInt, data: UInt, byteLane: UInt, isWrite: Bool): Unit = {
+        c.clock.step(1)
+        c.io.req.valid.poke(true.B)
+        c.io.req.bits.addrRequest.poke(addr)
+        c.io.req.bits.dataRequest.poke(data)
+        c.io.req.bits.activeByteLane.poke(byteLane)
+        c.io.req.bits.isWrite.poke(isWrite)
+        c.clock.step(1)
+        c.io.req.valid.poke(false.B)
+      }
+    }
+  }
+
+  "write ls half word, read ls half word in maskable bram" in {
+    val programFile = getFile
+    test(BlockRam.createMaskableRAM(programFile, WishboneConfig(32,32), 1024)).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
+      c.clock.step(2)
+      sendRequest(0.U, "habcdef01".U, "b0011".U, true.B)
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      while(c.io.rsp.valid.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      println("Got the response now sending a read request")
+
+      c.clock.step(2)
+      sendRequest(0.U, 0.U, "b0011".U, false.B)
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+
+
+      while(c.io.rsp.valid.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      println("Got the response now reading expected data")
+      c.io.rsp.bits.dataResponse.expect("h0000ef01".U)
+
+      println("EXPECTED DATA IS: " + "h0000ef01".U.litValue().toInt.toString + " GOT " + c.io.rsp.bits.dataResponse.peek().litValue().toInt.toString)
+      c.clock.step(8)
+
+      def sendRequest(addr: UInt, data: UInt, byteLane: UInt, isWrite: Bool): Unit = {
+        c.clock.step(1)
+        c.io.req.valid.poke(true.B)
+        c.io.req.bits.addrRequest.poke(addr)
+        c.io.req.bits.dataRequest.poke(data)
+        c.io.req.bits.activeByteLane.poke(byteLane)
+        c.io.req.bits.isWrite.poke(isWrite)
+        c.clock.step(1)
+        c.io.req.valid.poke(false.B)
+      }
+    }
+  }
+
+  "write two half word, read full word in maskable bram" in {
+    val programFile = getFile
+    test(BlockRam.createMaskableRAM(programFile, WishboneConfig(32,32), 1024)).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
+      c.clock.step(2)
+      sendRequest(0.U, "h0000ef01".U, "b0011".U, true.B)
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      while(c.io.rsp.valid.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      println("Got the response now sending new write request")
+
+      sendRequest(0.U, "habcd0000".U, "b1100".U, true.B)
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      while(c.io.rsp.valid.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      println("Got the response now sending read request")
+
+      sendRequest(0.U, 0.U, "b1111".U, false.B)
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      while(c.io.rsp.valid.peek().litToBoolean != true) {
+        println("wait")
+        c.clock.step(1)
+      }
+      println("VALID RESPONSE = " + c.io.rsp.valid.peek().litToBoolean.toString)
+      println("Got the response now reading expected data")
+      c.io.rsp.bits.dataResponse.expect("habcdef01".U)
+
+      println("EXPECTED DATA IS: " + "habcdef01".U.litValue().toInt.toString + " GOT " + c.io.rsp.bits.dataResponse.peek().litValue().toInt.toString)
+      c.clock.step(8)
+
+      def sendRequest(addr: UInt, data: UInt, byteLane: UInt, isWrite: Bool): Unit = {
+        c.clock.step(1)
+        c.io.req.valid.poke(true.B)
+        c.io.req.bits.addrRequest.poke(addr)
+        c.io.req.bits.dataRequest.poke(data)
+        c.io.req.bits.activeByteLane.poke(byteLane)
+        c.io.req.bits.isWrite.poke(isWrite)
+        c.clock.step(1)
+        c.io.req.valid.poke(false.B)
+      }
+    }
+  }
 }
