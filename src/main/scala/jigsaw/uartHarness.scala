09fe31868fa1b174dd9b3dfb7e76d2cea3ffd13b
Shahzaib2028 <mohammadshahzaib2028@gmail.com>
2022-02-03 01:07:25 +0500

    update driver for both buses

--- src/main/scala/jigsaw/uartHarness.scala
+++ src/main/scala/jigsaw/uartHarness.scala
@@ -42,3 +42,42 @@ object UARTHarnessDriver extends App {
   implicit val config = WishboneConfig(32,32)
   println(ChiselStage.emitVerilog(new uartHarness()))
 }
+
+
+
+
+class uartHarness_TL(implicit val config: TilelinkConfig) extends Module {
+  val io = IO(new Bundle {
+
+    // bus interconnect interfaces
+    val req = Flipped(Decoupled(new TLRequest()))
+    val rsp = Decoupled(new TLResponse())
+
+    // UART interfaces
+
+    val cio_uart_rx_i = Input(Bool())
+    val cio_uart_tx_o = Output(Bool())
+    val cio_uart_intr_tx_o = Output(Bool())
+
+  })
+  val hostAdapter = Module(new TilelinkHost())
+  val deviceAdapter = Module(new TilelinkDevice())
+  val uart_wrapper = Module(new uart(new TLRequest(), new TLResponse()))
+
+  hostAdapter.io.reqIn <> io.req
+  io.rsp <> hostAdapter.io.rspOut
+  hostAdapter.io.tlMasterTransmitter <> deviceAdapter.io.tlMasterReceiver
+  hostAdapter.io.tlSlaveReceiver <> deviceAdapter.io.tlSlaveTransmitter
+
+  uart_wrapper.io.request <> deviceAdapter.io.reqOut
+  uart_wrapper.io.response <> deviceAdapter.io.rspIn
+
+    uart_wrapper.io.cio_uart_rx_i := io.cio_uart_rx_i
+    io.cio_uart_tx_o := uart_wrapper.io.cio_uart_tx_o
+    io.cio_uart_intr_tx_o := uart_wrapper.io.cio_uart_intr_tx_o
+}
+
+object UARTHarnessDriverTL extends App {
+  implicit val config = TilelinkConfig()
+  println(ChiselStage.emitVerilog(new uartHarness_TL()))
+}
