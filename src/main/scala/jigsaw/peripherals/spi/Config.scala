555be7da2aba0bd3b569cb353058be1ad4a995db
Talha Ahmed <talhaahmedashraf@gmail.com>
2022-02-03 00:20:19 +0500

    remove unnecessary files

--- src/main/scala/jigsaw/peripherals/spi/Config.scala
+++
@@ -1,9 +0,0 @@
-package jigsaw.peripherals.spi
-
-case class Config(
-    CLK_FREQUENCE : Int = 15000000,
-    SPI_FREQUENCE : Int = 5000000,
-    CPOL          : Int = 1,
-    CPHA          : Int = 1,
-    DW            : Int = 32
-)
